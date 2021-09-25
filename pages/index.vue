2e726c0b467fa9d2a4474c19da324f5d30fecbe7
kevin regenrek <hello@regenrek.at>
2021-09-25 07:58:43 +0000

    initial

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <Bloks :bloks="story.content.bloks" />
+</template>
+<script>
+import { story } from "~/__mocks__/story";
+
+export default {
+  data() {
+    return {
+      story,
+    };
+  },
+  components: {},
+  //   async asyncData({ $storyapi, error, route }) {
+  //     try {
+
+  //         const fullSlug = (route.path === '/' || route.path === '') ? 'home' : route.path
+
+  //         const currentPage = await $storyapi.getStory(
+  //             fullSlug,
+  //             {
+  //                 resolve_links: 'url'
+  //             }
+  //         )
+
+  //         return {
+  //             story: currentPage.data.story
+  //         }
+  //     } catch (e) {
+  //         console.error('Exception', e)
+  //         error({
+  //             statusCode: 404,
+  //             message: e.message
+  //         })
+  //     }
+  // }
+};
+</script>
